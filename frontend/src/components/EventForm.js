ebe7dd1e837f648d56887df74a2d6df645fbd73a
parth <parth@advant.xyz>
2026-02-26 23:00:00 +0530

    add form submitting state

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -6,6 +6,10 @@ import classes from "./EventForm.module.css";
 function EventForm({ method, event }) {
   const navigate = useNavigate();
 
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   function cancelHandler() {
     navigate("..");
   }
@@ -53,10 +57,12 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Save"}
+        </button>
       </div>
     </Form>
   );
